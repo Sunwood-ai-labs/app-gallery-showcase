4dbc5aeb12ed4c1573cbf853889c9f34ec337caf
Maki <sunwood.ai.labs@gmail.com>
2025-01-23 19:45:49 +0900

    ♻️ refactor: デモスペースデータの改善
    - デモスペースデータに`author`オブジェクトを追加。
    - `author`オブジェクトには、`name`, `username`, `image`プロパティが含まれる。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,28 +12,44 @@ const SpaceCard = dynamic(() => import('../components/SpaceCard'), {
 const demoSpaces = [
   {
     title: 'Kokoro TTS',
-    author: 'hexgrad',
+    author: {
+      name: 'hexgrad',
+      username: 'hexgrad',
+      image: 'https://avatars.githubusercontent.com/u/example1'
+    },
     likes: 1220,
     daysAgo: 5,
     runtime: 'ZENO'
   },
   {
     title: 'TransPixar',
-    author: 'wilkemang',
+    author: {
+      name: 'wilkemang',
+      username: 'wilkemang',
+      image: 'https://avatars.githubusercontent.com/u/example2'
+    },
     likes: 874,
     daysAgo: 12,
     runtime: 'ZENO'
   },
   {
     title: 'FitDIT',
-    author: 'BoyuanJiang',
+    author: {
+      name: 'BoyuanJiang',
+      username: 'BoyuanJiang',
+      image: 'https://avatars.githubusercontent.com/u/example3'
+    },
     likes: 125,
     daysAgo: 3,
     runtime: 'ZENO'
   },
   {
     title: 'VITPose Transformers',
-    author: 'hysts',
+    author: {
+      name: 'hysts',
+      username: 'hysts',
+      image: 'https://avatars.githubusercontent.com/u/example4'
+    },
     likes: 68,
     daysAgo: 2,
     runtime: 'ZENO'
